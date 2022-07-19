cf1c35dc8857a21faa9a5216b7cdb5d9c55efdc4
pedroaqo <82339371+pedroaqo@users.noreply.github.com>
2022-07-19 14:49:46 -0300

    Add files via upload

---
+++ dataset.py
@@ -0,0 +1,32 @@
+import config
+import os
+from torch.utils.data import Dataset, DataLoader
+import cv2
+
+
+class MapDataset(Dataset):
+    def __init__(self, root_dir):
+        self.root_dir = root_dir
+        self.list_files = os.listdir(self.root_dir)
+
+    def __len__(self):
+        return len(self.list_files)
+
+    def __getitem__(self, index):
+        img_file = self.list_files[index]
+        img_path = os.path.join(self.root_dir, img_file)
+        image = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
+
+        input_image = image[:, 776:, :]
+        target_image = image[:, :776, :]
+
+        augmentations = config.both_transform(image=input_image, image0=target_image)
+        input_image = augmentations["image"]
+        target_image = augmentations["image0"]
+
+        input_image = config.transform_only_input(image=input_image)["image"]
+        target_image = config.transform_only_mask(image=target_image)["image"]
+
+        return input_image, target_image
+
+
